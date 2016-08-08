83acaf2cf6fceed997d5c16cbd01c523113dd822
birthbyr <r.kimura@sein.mse.kyutech.ac.jp>
2016-08-08 19:47:14 +0900

    fix to_csv method for excel

--- kintai_app/app/models/kintai.rb
+++ kintai_app/app/models/kintai.rb
@@ -1,7 +1,7 @@
 class Kintai < ActiveRecord::Base
   
-  def self.to_csv
-    CSV.generate do |csv|
+  def self.to_csv(options = {})
+    CSV.generate(options) do |csv|
       
       csv << column_names
       all.each do |kintai|
