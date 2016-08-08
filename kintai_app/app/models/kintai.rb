903dcd1f5628e283a9c01c5703a0634f0f605564
birthbyr <r.kimura@sein.mse.kyutech.ac.jp>
2016-08-08 17:38:45 +0900

    add csv method

--- kintai_app/app/models/kintai.rb
+++ kintai_app/app/models/kintai.rb
@@ -1,2 +1,13 @@
 class Kintai < ActiveRecord::Base
+  
+  def self.to_csv
+    CSV.generate do |csv|
+      
+      csv << column_names
+      all.each do |kintai|
+        csv << kintai.attributes.values_at(*column_names)
+      end
+    end
+    
+    
 end
